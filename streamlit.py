e8a22cc3cc1400f88f1173ef0e330029bc6822e6
ingridHansen <r0879034@student.thomasmore.be>
2024-06-13 21:28:29 +0200

    Should work

--- streamlit.py
+++ streamlit.py
@@ -85,7 +85,7 @@ with EDA:
 with Model:
         st.header("Classify your comment")
         # Load the model
-        model_directory = 'NLPStreamlit/model'
+        model_directory = './model'
         model = AutoModelForSequenceClassification.from_pretrained(model_directory, local_files_only=True)
         tokenizer = AutoTokenizer.from_pretrained(model_directory, local_files_only=True)
         pipeline =  TextClassificationPipeline(model=model, tokenizer=tokenizer)
